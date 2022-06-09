5da38842197bf6b9044e820864f5cb70f284ad69
John Purcell <john@caveofprogramming.com>
2022-06-09 20:16:59 +0200

    P01190 A Program That Does a Calculation

---
+++ calculation.py
@@ -0,0 +1,8 @@
+#!./venv/bin/python
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+years_left = 100 - int(age)
+
+print("You have " + str(years_left) + " years to reach 100 years old.")
