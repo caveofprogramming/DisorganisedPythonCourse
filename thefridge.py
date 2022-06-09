c6e0e705ca2579fc7006b40b28818bdd256f3de1
John Purcell <john@caveofprogramming.com>
2022-06-09 23:36:37 +0200

    P02070 The Fridge Exercise and Comments

---
+++ thefridge.py
@@ -0,0 +1,13 @@
+#!./venv/bin/python
+
+# Exercise: The Fridge
+
+"""
+Get the user to input a temperature in celsius.
+
+If the temperature is less than 0 degrees, output "Too cold."
+If it's between 0 and 4 degrees, output "OK"
+If it's between 4 and 6 degrees, output "Too warm"
+If over 6 degrees, then output "Fridge broken"
+"""
+
