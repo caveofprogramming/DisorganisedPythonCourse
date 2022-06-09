c6b2a4093111321451cbfc6b6fb51c754b130a25
John Purcell <john@caveofprogramming.com>
2022-06-09 21:43:06 +0200

    P02030 The If Statement

---
+++ booleans.py
@@ -0,0 +1,11 @@
+#!./venv/bin/python
+
+value = False
+
+print(type(value))
+
+greeting = "Hello"
+
+is_hello = greeting == "Hellooo"
+
+print(is_hello)
---
+++ if.py
@@ -0,0 +1,7 @@
+#!./venv/bin/python
+
+name = input("Enter your name: ")
+
+if name == "John":
+    print("Name is John")
+    print("Name was correct")
