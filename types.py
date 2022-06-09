4839ff7dad9913ab4eb2cabb210050ffaed1ec98
John Purcell <john@caveofprogramming.com>
2022-06-09 20:07:57 +0200

    P01180 Python Types

---
+++ types.py
@@ -0,0 +1,15 @@
+#!./venv/bin/python
+
+width = 15
+
+print(type(width))
+
+length = 22.5
+
+print(type(length))
+
+area = width * length;
+
+print(type("Area of land: "))
+
+print("Area of land: " + str(area))
