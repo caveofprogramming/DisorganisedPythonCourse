5053658f4fcba13c26ceb21e565791cd02c85a63
John Purcell <john@caveofprogramming.com>
2022-06-09 23:53:08 +0200

    P02080 The Fridge Exercise Solution

--- thefridgesolution.py
+++ thefridgesolution.py
@@ -10,3 +10,18 @@ If it's between 0 and 4 degrees, output "OK"
 If it's between 4 and 6 degrees, output "Too warm"
 If over 6 degrees, then output "Fridge broken"
 """
+
+temperature = input("Enter fridge temperature > ")
+
+if float(temperature) < 0:
+    print("Too cold")
+elif float(temperature) < 4:
+    print("OK")
+elif float(temperature) < 6:
+    print("Too warm")
+else:
+    print("Fridge broken")
+
+
+
+
