d48d091136faf3fb8b3f8cb21579e206db2dcdd4
John Purcell <john@caveofprogramming.com>
2022-06-09 23:21:33 +0200

    P02060 Comparison Operators

---
+++ comparisons.py
@@ -0,0 +1,16 @@
+#!./venv/bin/python
+
+print("cat" == "cat")
+print(4 == 5)
+print(3 < 4)
+print(3 > 4)
+print("cat" > "dog")
+
+print(ord("D"))
+print(ord("d"))
+
+print("cat" != "dog")
+print(4 != 4)
+
+print(3 >= 3)
+print(3 <= 4)
