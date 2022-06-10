4a556293ee3ba9a8a33fbb5ea5357a9aa2a41d42
John Purcell <john@caveofprogramming.com>
2022-06-11 00:42:49 +0200

    P03090 Factorial Exercise

---
+++ factorial_exercise.py
@@ -0,0 +1,23 @@
+#!./venv/bin/python
+
+"""
+Write a function that calculates the factorial of a number.
+
+The factorial of a number is defined like this:
+
+n! = n * (n - 1) (n - 2) * ... * 3 * 2 * 1
+
+e.g. 
+
+3! = 3 * 2 * 1
+5! = 5 * 4 * 3 * 2 * 1
+
+The factorial of zero, 0!, is defined as 1.
+
+Calcuate the factorial of 7 , i.e. 7!
+"""
+
+def main():
+    return
+    
+main()
