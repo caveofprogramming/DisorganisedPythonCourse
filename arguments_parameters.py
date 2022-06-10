19e970e56c3c078359bd5923c6e3db02e6f96e11
John Purcell <john@caveofprogramming.com>
2022-06-10 23:11:22 +0200

    P03040 Parameters and Arguments

--- arguments_parameters.py
+++ arguments_parameters.py
@@ -1,10 +1,11 @@
 #!./venv/bin/python
 
-def greet(name):
-    print("Hello " + name)
+def greet(user):
+    print("Hello " + user)
 
 def main():
     name = input("What's your name? > ")
     greet(name)
 
 main()
+
