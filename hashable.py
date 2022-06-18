3a6babd13a6f3639b030d780bddbf074203855e9
John Purcell <john@caveofprogramming.com>
2022-06-18 16:26:19 +0200

    P04340 Hashing

---
+++ hashable.py
@@ -0,0 +1,16 @@
+#!./venv/bin/python
+
+def main():
+
+    # Hashtable algorithms
+
+    # Hash number
+
+   print(hash("hello"))
+
+   print(hash((1, 2, 3)))
+   # print(hash((1, [], 3)))
+
+   test = {1, 2, []}
+    
+main()
