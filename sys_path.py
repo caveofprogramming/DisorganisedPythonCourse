822089fc37dd84f5614f0f6addf5f5dfcc083d5c
John Purcell <john@caveofprogramming.com>
2022-06-21 13:29:02 +0200

    P06010 The Sys Path

---
+++ sys_path.py
@@ -0,0 +1,13 @@
+#!./venv/bin/python
+
+# If current directory is the first directory in the module search
+# path and it's causing problems, we can remove it as follows.
+# import sys
+# sys.path.pop(0)
+
+# But better to just not have python programs with names the same
+# as built-in modules in the current directory.
+
+# So, no "types.py" for instance.
+
+import re
