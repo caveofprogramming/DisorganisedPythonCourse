88055d74c5036d67fda80fc98a378bbe43ce8858
John Purcell <john@caveofprogramming.com>
2022-07-21 05:39:43 +0200

    P11140 Bitwise Colours Exercise

---
+++ op_color_exercise.py
@@ -0,0 +1,20 @@
+
+"""
+red     0x12
+green   0x34
+blue    0x56
+
+0x123456 (combined colour)
+
+Write a function that accepts three colors: red, green and blue.
+The function returns a single combined number containing all three colours
+
+to_color(red, green, blue)
+
+Write a function that accept a color and splits it into red, green and blue components
+
+from_color(color):
+    # do something
+    return red, blue, green
+
+"""
