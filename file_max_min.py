2abf6c4388a8b82a04d8ba27f0fb72774079f603
John Purcell <john@caveofprogramming.com>
2022-08-09 19:32:20 +0200

    P13150 Storing Numbers in Bytes

--- file_max_min.py
+++ file_max_min.py
@@ -11,20 +11,25 @@
 111 7
 """
 
+
 def main():
-    print(pow(2, 3) - 1)
-    print(pow(2, 2) - 1)
+   print(pow(2, 3) - 1)
+   print(pow(2, 2) - 1)
+   print()
+   print(pow(2, 8) - 1)
+   print(pow(2, 7) - 1)
+   print()
+   print(pow(2, 16) - 1)
+   print(pow(2, 15) - 1)
+   print()
+   print(0xFF)
+   print(0xFFFF)
+   print()
 
-    print(pow(2, 8) - 1)
-    print(pow(2, 7) - 1)
+   # 4 bytes    7 digits
+   # 8 bytes    16 digits
 
-    print(pow(2, 16) - 1)
-    print(pow(2, 15) - 1)
-
-    # 123.456
-
-    # 4 bytes : 7 after the decimal point
-    # 8 bytes : 15 digits after the decimal point
+   # 4 bytes 12.34567
 
 if __name__ == "__main__":  
     main()
