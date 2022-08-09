e52f541b355289843303060d4bb4bf26b7299fd2
John Purcell <john@caveofprogramming.com>
2022-08-09 19:31:15 +0200

    P13170 Saving and Loading Arrays UPDATED

--- file_saving_arrays.py
+++ file_saving_arrays.py
@@ -1,26 +1,26 @@
 #!./venv/bin/python
 
-# array python python.org
-
 from array import array
 
 def main():
-    numbers = [1.23, 0.0001, -5.678, 4.01, 512.23, 1.123456789]
-    number_values = len(numbers)
-    filename = "numbers.dat"
+    numbers = array('f', [-8, 123.0, 0.0001, 87.5, 1.23456789])
+    NUMBER_ITEMS = len(numbers)
 
-    values = array('f', numbers)
+    filename = "numbers.bin"
 
     with open(filename, 'wb') as file:
-        values.tofile(file)
+        numbers.tofile(file)
 
-    del values
+    del numbers
 
     with open(filename, 'rb') as file:
-        values = array('f')
-        values.fromfile(file, number_values)
+        numbers = array('f')
+        numbers.fromfile(file, NUMBER_ITEMS)
 
-    print(list(values))
+    print(numbers)
+
+
+    print("Finished.")
 
 if __name__ == "__main__":  
     main()
