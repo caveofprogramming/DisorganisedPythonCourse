b068a2bc8d1f55586c18f3aeee3dddaae360f8cb
John Purcell <john@caveofprogramming.com>
2022-08-26 15:51:07 +0200

    P14160 Bessel Solution

--- np_bessel_solution.py
+++ np_bessel_solution.py
@@ -5,9 +5,9 @@ import numpy as np
 """
 Show that the average value of the variance of small samples actually improves if we use n-1 in the variance formula instead of n
 
-Show that duplicating entries in an array of random values doesn't change if we add duplicate values to the array.
+Show that duplicating entries in an array of random values doesn't change the variance if we add duplicate values to the array.
 
-How does variance change if we multiple the entries in a random array by a scalar? How does standard deviation change?
+How does variance change if we multiply the entries in a random array by a scalar? How does standard deviation change?
 
 In a (large) array of normally-distributed values with a variance of 1 and a mean of 0, what percentage of the values lie between -1 and 1?
 """
