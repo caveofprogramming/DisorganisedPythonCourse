ae7a07255de1b045d9e8b02363484f924c0931ce
John Purcell <john@caveofprogramming.com>
2022-09-07 18:54:10 +0200

    P17020 Weight Data

---
+++ rg_weight_data.py
@@ -0,0 +1,18 @@
+#!./venv/bin/python
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def main():
+    df = pd.read_csv("weights.csv", index_col=0, sep='\s+')
+    
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    fig.suptitle("Weight vs Time")
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Weight(Kg)")
+    ax.plot(df.index, df['weight'])
+    plt.show()
+
+if __name__ == "__main__":  
+    main()
