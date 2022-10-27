6dab2e762f53153d82e49a422f3a44a69fe7c391
John Purcell <john@caveofprogramming.com>
2022-10-27 09:40:51 +0200

    P18170 The Linkage Table

--- cl_dendrogram.py
+++ cl_dendrogram.py
@@ -3,11 +3,14 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
-data = np.array([0, 1, 11, 12]).reshape(-1, 1)
+data = np.array([0, 1, 2, 12]).reshape(-1, 1)
 
 X = StandardScaler().fit_transform(data)
 
 clusters = linkage(X)
+
+print(X)
+print(clusters)
 
 dendrogram(clusters)
 
