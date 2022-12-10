90e3d2f85abb09d058cd28dffb017f06b60ebcc8
John Purcell <john@caveofprogramming.com>
2022-12-10 04:04:10 +0100

    22080 MNIST ANN Solution Part 2

--- nn_mnist_solution.py
+++ nn_mnist_solution.py
@@ -6,6 +6,10 @@ Display a chart showing only digits that were NOT recognised correctly.
 from keras.datasets import mnist
 from keras.utils import to_categorical
 from sklearn.preprocessing import StandardScaler
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from sklearn.metrics import accuracy_score
+import numpy as np
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
@@ -19,4 +23,17 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
+model = Sequential()
+model.add(Dense(1000, input_dim=len(X_train[0]), activation='relu'))
+model.add(Dense(len(y_train[0]), activation='softmax'))
 
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics='accuracy')
+
+model.fit(X_train, y_train, epochs=20, batch_size=128)
+
+y_predicted = model.predict(X_test)
+
+y_test = np.argmax(y_test, axis=1)
+y_predicted = np.argmax(y_predicted, axis=1)
+
+print("Score:", accuracy_score(y_test, y_predicted))
